51a47b07bd09571889409d08a6e3d0d3ae6ac48e
gabi-bocardi <bocardigm@gmail.com>
2023-03-30 20:51:53 -0700

    Working in eslint rules and installation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,8 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   'extends': [
-    'plugin:vue/vue3-essential',
+    'plugin: vue/base',
+    'plugin:vue/vue3-strongly-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript'
   ],
