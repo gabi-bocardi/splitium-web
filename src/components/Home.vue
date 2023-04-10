8ea1b799464265ba25ff2528e6b68fe7a3611de4
gabi-bocardi <bocardigm@gmail.com>
2023-04-09 22:01:33 -0700

    Click name show user info

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,16 @@
 <template>
     <v-container v-if="user" class="d-flex flex-column align-center w-100">
-        <h1 class="text-h2  align-center" >Welcome {{ user.name }}!</h1>
+        <h1 class="text-h2  align-center">Welcome 
+            <span @click="isShowUserInfo = !isShowUserInfo" className="name">
+                {{ user.name }}!
+            </span>
+        </h1>
+        <v-container v-if="isShowUserInfo" class="d-flex flex-column align-center w-100 mt-5">
+
+            <p  class="text-h5"> Username: {{ user.username }} </p>
+            <p  class="text-h5"> User ID: {{ user.id }}</p>
+        </v-container>
+
        <GroupCard v-for="(group, index) in userGroups" :key="index" :group="group"/>
        <p v-if="!userGroups.length" class="text-h4 mt-10"> You have no groups yet.</p>
     </v-container>
@@ -20,6 +30,7 @@ export default {
         return{
             user: {} as IUser,
             userGroups: [] as IGroup[],
+            isShowUserInfo: false,
         }
     },
     methods: {
@@ -50,3 +61,12 @@ export default {
 }
 
 </script>
+<style scoped>
+
+.name{
+    cursor: pointer;
+    color: #6A1B9A;
+    text-decoration: underline;
+}
+
+</style>
